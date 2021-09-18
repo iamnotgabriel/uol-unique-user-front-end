4c0add53d464554dbcbea235fbb581eeb2a98eaf
Victoria <victoria.andujar10@gmail.com>
2021-09-17 23:04:59 -0300

    listagem de usuarios

---
+++ src/components/Header/styles.ts
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  width: 100%;
+  background: var(--primary-color);
+  padding: 15px;
+
+  h3 {
+    color: var(--color-white);
+  }
+`;
--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,11 +1,57 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import Table, { ColumnsType } from "antd/lib/table";
+import api from "../../services/api";
 
-import { Container } from "./styles";
+import Header from "../../components/Header";
+
+import { Container, ContTable } from "./styles";
+
+interface UsersProps {
+  deviceId: string;
+  email: string;
+  id: number;
+  name: string;
+}
 
 const Dashboard: React.FC = () => {
+  const [users, setUsers] = useState<UsersProps[]>([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    async function loadUsers() {
+      setLoading(true);
+      const response = await api.get("/users");
+      const { data } = response;
+
+      setUsers(data);
+      setLoading(false);
+    }
+
+    loadUsers();
+  }, []);
+
+  const columns: ColumnsType<UsersProps> = [
+    {
+      title: "Device ID",
+      dataIndex: "deviceId",
+    },
+    {
+      title: "Nome",
+      dataIndex: "name",
+    },
+    {
+      title: "E-mail",
+      dataIndex: "email",
+    },
+  ];
   return (
     <Container>
-      <h1>Dashboard</h1>
+      <Header />
+
+      <ContTable>
+        <h2>Usuários</h2>
+        <Table columns={columns} dataSource={users} loading={loading} />
+      </ContTable>
     </Container>
   );
 };
