34b0a8a11875b8f3388b1410a33793f63cce3cf2
victoriandujar <victoria.andujar10@gmail.com>
2021-10-06 09:35:42 -0300

    chore: Reajustes

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,21 +1,43 @@
 import React, { useState, useEffect } from "react";
 import Table, { ColumnsType } from "antd/lib/table";
+
+import { Typography } from "antd";
+import { format } from "date-fns";
 import api from "../../services/api";
 
 import Header from "../../components/Header";
 
-import { Container, ContTable } from "./styles";
+import { Container, ContTable, Content } from "./styles";
 
 interface UsersProps {
-  deviceId: string;
   email: string;
   id: number;
   name: string;
+  phone: string;
+  initialTime: string;
+  endTime: string;
+  signUp: {
+    deviceId: string;
+    browser: string;
+    cpuCores: number;
+    deviceName: string;
+    endTime: string;
+    gpuName: string;
+    ip: string;
+    os: string;
+    pasteCount: number;
+    screenHeight: number;
+    screenWidth: number;
+    startTime: string;
+    timezone: string;
+  };
 }
 
 const Dashboard: React.FC = () => {
   const [users, setUsers] = useState<UsersProps[]>([]);
   const [loading, setLoading] = useState(false);
+
+  const { Title, Text } = Typography;
 
   useEffect(() => {
     async function loadUsers() {
@@ -23,17 +45,34 @@ const Dashboard: React.FC = () => {
       const response = await api.get("/users");
       const { data } = response;
 
-      setUsers(data);
+      const resultsUsersFormatted = () => {
+        const dataFormat = data.map((usersForm: UsersProps) => {
+          const followFormatted = {
+            ...usersForm,
+            initialTime: formatterDate(new Date(usersForm.signUp.startTime)),
+            endTime: formatterDate(new Date(usersForm.signUp.endTime)),
+          };
+
+          return followFormatted;
+        });
+        return dataFormat;
+      };
+
+      setUsers(resultsUsersFormatted);
       setLoading(false);
     }
 
     loadUsers();
   }, []);
 
+  const formatterDate = (value: Date) => {
+    return format(value, "dd/MM/yyyy 'às' HH:mm'h'");
+  };
+
   const columns: ColumnsType<UsersProps> = [
     {
-      title: "Device ID",
-      dataIndex: "deviceId",
+      title: "Número",
+      dataIndex: "id",
     },
     {
       title: "Nome",
@@ -43,6 +82,10 @@ const Dashboard: React.FC = () => {
       title: "E-mail",
       dataIndex: "email",
     },
+    {
+      title: "Telefone",
+      dataIndex: "phone",
+    },
   ];
   return (
     <Container>
@@ -50,7 +93,44 @@ const Dashboard: React.FC = () => {
 
       <ContTable>
         <h2>Usuários</h2>
-        <Table columns={columns} dataSource={users} loading={loading} />
+        <Table
+          columns={columns}
+          dataSource={users}
+          loading={loading}
+          size="middle"
+          rowKey={(key) => key.id}
+          expandable={{
+            expandedRowRender: (record) => (
+              <Content>
+                <div className="devices">
+                  <Title level={5}>Devices</Title>
+                  <Text>Device ID: {record.signUp.deviceId}</Text>
+                  <Text>Device Name: {record.signUp.deviceName}</Text>
+                </div>
+                <div className="pc">
+                  <Title level={5}>Dados do Computador</Title>
+                  <Text>IP: {record.signUp.ip}</Text>
+                  <Text>Browser: {record.signUp.browser}</Text>
+                  <Text>Sistema Operacional: {record.signUp.os}</Text>
+                </div>
+                <div>
+                  <Title level={5}>Hardware</Title>
+                  <Text>CPU Cores: {record.signUp.cpuCores}</Text>
+                  <Text>GPU: {record.signUp.gpuName}</Text>
+                  <Text>Altura do Monitor: {record.signUp.screenHeight}</Text>
+                  <Text>Largura do Monitor: {record.signUp.screenWidth}</Text>
+                </div>
+                <div>
+                  <Title level={5}>Momento do Cadastro</Title>
+                  <Text>Início: {record.initialTime}</Text>
+                  <Text>Fim: {record.endTime}</Text>
+                  <Text>Timezone: {record.signUp.timezone}</Text>
+                </div>
+              </Content>
+            ),
+            rowExpandable: (record) => record.name !== "Not Expandable",
+          }}
+        />
       </ContTable>
     </Container>
   );
--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -14,3 +14,19 @@ export const ContTable = styled.div`
     margin: 10px 0 15px 0;
   }
 `;
+
+export const Content = styled.div`
+  display: flex;
+  justify-content: space-between;
+
+  div {
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+  }
+
+  /* .devices,
+  .pc {
+    margin-right: 50px;
+  } */
+`;
